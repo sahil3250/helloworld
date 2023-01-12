689019075d3a112712bed13cc69e2b71d85aaf33
sahil3250 <118569353+sahil3250@users.noreply.github.com>
2023-01-12 13:25:20 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/**********************************************************************************  
+ * WEB322 – Assignment 1* 
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  *  
+ * No part of this assignment has been copied manually or electronically from any other source*  (including web sites)
+ *  or distributed to other students.* * 
+ *  Name: ___________sahilpreet singh___________ 
+ * Student ID: _______166445213_______ 
+ * Date: ___12/01/2023_____________*
+ * *  Online (Cyclic) URL: ______https://light-blazer-wasp.cyclic.app/________________________________________________
+ * *********************************************************************************/ 
+
+
+
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
